e77f8073c3a5cc06505daddf33c375145ad52bf5
Youness BENNAJ <youness.bennaj@gmail.com>
2019-06-17 22:47:16 +0200

    enable fetching data from the dialog flow api

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React, { Component } from 'react';
 import MessageList from './components/messageList';
 import SendMessageForm from './components/sendMessageForm';
 import 'bootstrap/dist/css/bootstrap.css';
-import '@fortawesome/fontawesome-free/css/fontawesome.css'
+import '@fortawesome/fontawesome-free/css/fontawesome.css';
+import axios from 'axios';
 const messageList = [
     {
-        name: 'John',
+        name: 'User',
         text: 'Hello',
         id: Math.random()
     },
     {
-        name: 'Joey',
-        text: 'Hi !',
+        name: 'Bot',
+        text: 'Hi ! How I can help you ?',
         id: Math.random()
     }
 ]
@@ -20,11 +21,25 @@ class App extends Component {
 
     message = React.createRef();
 
-    sendMessage = (event, text) => {
-        const messages = this.state.messages;
-        messages.push({ name: 'Teddy', text, id: Math.random() });
-        this.setState({ messages });
+
+    receiveMessage = async (event, text) => {
         event.preventDefault();
+        const data = { text };
+        this.sendMessage(text, 'User');
+        const response = await axios.post('https://d5b785a5.ngrok.io/api/df_text_query', data);
+        const botMessage = response.data.fulfillmentMessages[0].text.text[0];
+        this.sendMessage(botMessage, 'Bot');
+    }
+
+    sendMessage(text, name) {
+        const message = {
+            name: name,
+            text,
+            id: Math.random()
+        };
+        const messages = this.state.messages;
+        messages.push(message);
+        this.setState({ messages });
     }
 
     render() {
@@ -32,7 +47,7 @@ class App extends Component {
             <main className="container">
                 <h1 className="text-center">Hospitality Chatbot</h1>
                 <MessageList messages={this.state.messages} />
-                <SendMessageForm onSubmit={this.sendMessage} />
+                <SendMessageForm onSubmit={this.receiveMessage} />
             </main>);
     }
 }
