1cea578f01906d6dec4a0ea84805aac6e455290d
Youness BENNAJ <youness.bennaj@gmail.com>
2019-06-18 01:44:23 +0200

    apply basic style for the component

--- src/components/message.jsx
+++ src/components/message.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
 
 const Message = (props) => {
+    const liStyle = {
+        listStyleType: 'none'
+    };
+
+    const cardStyle = {
+        borderRadius: '18px 18px 18px 0px'
+    }
+
     return (
-        <li className="list-group-item">{props.message.name}: {props.message.text}</li>
+        <li style={liStyle}>
+            <div className="row">
+                <div className="d-flex align-items-center mr-2">
+                    <img src="https://i.pravatar.cc/50" alt="avatar" className=" rounded-circle pull-left" />
+                </div>
+                <div className="card" style={cardStyle}>
+                    <div className="card-body">{props.message.text}</div>
+                </div>
+            </div>
+        </li>
     );
 }
 
