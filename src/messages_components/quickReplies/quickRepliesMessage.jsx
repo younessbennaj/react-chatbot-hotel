58459ba1e205a336f47ec0a6f287a3e2ce3bc8ce
Youness BENNAJ <youness.bennaj@gmail.com>
2019-06-28 14:20:25 -0400

    init the quick replies component

---
+++ src/messages_components/quickReplies/quickRepliesMessage.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class quickRepliesMessage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+    render() {
+        return (
+            <h2>Quick Replies</h2>
+        );
+    }
+}
+
+export default quickRepliesMessage;
